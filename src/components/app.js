e7f51d2c11949e676c94aecf84c9eb18c5c0cc53
A.D <adrianlebaron@gmail.com>
2018-12-14 12:50:16 -0700

    added all the files before export and importing themk

---
+++ src/components/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Calculator from "calculator";
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>DevCamp React Starter</h1>
+        <h2>React Redux Router</h2>
+        <Calculator />
+      </div>
+    );
+  }
+}
---
+++ src/components/calculatorKey.js
@@ -0,0 +1,19 @@
+class CalculatorKey extends React.Component {
+  render() {
+    const { onPress, className, ...props } = this.props
+    
+    return (
+      <PointTarget onPoint={onPress}>
+        <button className={`calculator-key ${className}`} {...props}/>
+      </PointTarget>
+    )
+  }
+}
+
+const CalculatorOperations = {
+  '/': (prevValue, nextValue) => prevValue / nextValue,
+  '*': (prevValue, nextValue) => prevValue * nextValue,
+  '+': (prevValue, nextValue) => prevValue + nextValue,
+  '-': (prevValue, nextValue) => prevValue - nextValue,
+  '=': (prevValue, nextValue) => nextValue
+}
